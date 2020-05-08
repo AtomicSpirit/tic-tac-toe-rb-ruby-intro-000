cd281f2dc9e2082f9be800d9d5b9568f28f9e107
Daniel Park <park.daniel1@gmail.com>
2020-05-08 05:28:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,10 +40,8 @@ end
 end
 
 def valid_move?(board, index)
-  if (position_taken?(board, index) == false ) && (index.between?(0, 8) == true)
+  if !position_taken?(board, index)  && index.between?(0, 8)
     return true
-  else
-    return false
 end
 end
 
