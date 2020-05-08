91c38c39f2db121da48d2fb4e3a339bf2a00e5da
Daniel Park <park.daniel1@gmail.com>
2020-05-08 05:15:30 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,74 @@
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+  return index
+end
+
+def move(board, index, player_token)   # Either player_token is x or o.
+  board[index] = player_token
+  if player_token == "X" || player_token == "x"
+    return "X"
+  elsif player_token == "O" || player_token == "o"
+    return "O"
+end
+end
+
+def position_taken?(board, index)
+  if board[index] != " " && board[index] != ""
+  return true
+else
+  return false
+end
+end
+
+def valid_move?(board, index)
+  if (position_taken?(board, index) == false ) && (index.between?(0, 8) == true)
+    return true
+  else
+    return false
+end
+end
+
+
+def turn (board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index) == true
+    move(board, index, current_player(board))
+    turn(board)
+  end
+  return display_board(board)
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |spaces|
+    if spaces == "X" || "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "O" : "X"
+end
